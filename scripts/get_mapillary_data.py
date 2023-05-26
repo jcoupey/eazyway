1433a3bd200c39362dfbff86c47a34c23e21c77a
jcoupey <git@coupey.fr>
2023-05-26 11:38:34 +0200

    Python script to prefetch mapillary images in a small area.

---
+++ scripts/get_mapillary_data.py
@@ -0,0 +1,44 @@
+import json, mercantile, requests, sys
+from vt2geojson.tools import vt_bytes_to_geojson
+
+EAST = 2.37949
+WEST = 2.31293
+SOUTH = 48.80223
+NORTH = 48.82973
+
+
+def feature_ok(f, org_id):
+    return (
+        f["geometry"]["type"] == "Point"
+        and "organization_id" in f["properties"]
+        and f["properties"]["organization_id"] == org_id
+    )
+
+
+def get_filtered_data(access_token, org_id):
+    bounds = (WEST, SOUTH, EAST, NORTH)
+    tiles = list(mercantile.tiles(*bounds, zooms=14))
+
+    filtered_geojson = {"type": "FeatureCollection", "features": []}
+    for tile in tiles:
+        tile_url = "https://tiles.mapillary.com/maps/vtp/mly1_public/2/{}/{}/{}?access_token={}".format(
+            tile.z, tile.x, tile.y, access_token
+        )
+        response = requests.get(tile_url)
+        data = vt_bytes_to_geojson(response.content, tile.x, tile.y, tile.z)
+
+        filtered_geojson["features"].extend(
+            [f for f in data["features"] if feature_ok(f, org_id)]
+        )
+
+    return filtered_geojson
+
+
+if __name__ == "__main__":
+    access_token = sys.argv[1]
+    org_id = int(sys.argv[2])
+
+    filtered_data = get_filtered_data(access_token, org_id)
+
+    with open("data/mapillary.json", "w") as out:
+        json.dump(filtered_data, out)
