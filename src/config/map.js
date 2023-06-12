a576e90191a629ca948d65f48752aaf7baff8187
jcoupey <git@coupey.fr>
2023-06-12 17:33:07 +0200

    Put Mapillary view in dedicated div outside the map.

--- src/config/map.js
+++ src/config/map.js
@@ -4,7 +4,7 @@ var map = new maplibregl.Map({
   container: 'map', // container id
   style: 'https://vecto.teritorio.xyz/styles/basic/style.json?key=your_key',
   center: [2.34621, 48.81598], // starting position
-  zoom: 14 // starting zoom
+  zoom: 13 // starting zoom
 });
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -105,21 +105,4 @@ map.on('load', function () {
   });
 });
 
-class MapillaryControl {
-  onAdd(map) {
-    this._map = map;
-    this._container = document.createElement('div');
-    this._container.id = 'mjs';
-    this._container.className = 'maplibregl-ctrl';
-    return this._container;
-  }
-
-  onRemove() {
-    this._container.parentNode.removeChild(this._container);
-    this._map = undefined;
-  }
-};
-
-map.addControl(new MapillaryControl());
-
 viewer.init(map);
