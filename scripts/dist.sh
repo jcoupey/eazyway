96d4f33c537c1d5bb4f5f8ddc330b09f33b59f42
jcoupey <git@coupey.fr>
2023-06-27 16:40:58 +0200

    Move dist commands to dedicated script file.

---
+++ scripts/dist.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+npm run build
+rm bundle.raw.js
+
+rm -rf dist
+
+mkdir -p dist/img
+mkdir dist/css
+
+mv -v bundle.js dist
+cp -v img/danger.png dist/img
+cp -v img/image_marker.svg dist/img
+cp -v index.html dist
+cp -v css/eazyway.css dist/css
+
+sed -i 's|src/index.js|bundle.js|' dist/index.html
