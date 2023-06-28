d4e0dea1b675d8f0db0ab83d979a97524b904c3e
jcoupey <git@coupey.fr>
2023-06-28 15:04:48 +0200

    Remove map hash option.

--- src/config/map.js
+++ src/config/map.js
@@ -5,8 +5,7 @@ const style = require('./style.js');
 var map = new maplibregl.Map({
   container: 'map', // container id
   center: [2.34621, 48.81598], // starting position
-  zoom: 13, // starting zoom
-  hash: true
+  zoom: 13 // starting zoom
 });
 
 style.set(map);
