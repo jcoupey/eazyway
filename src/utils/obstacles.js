401c66f0e7f7729abfd4d17ab706efc28e471a89
jcoupey <git@coupey.fr>
2023-06-29 17:59:51 +0200

    Obstacles layers refactoring.

--- src/utils/obstacles.js
+++ src/utils/obstacles.js
@@ -49,115 +49,68 @@ var plotAround = function(map, geojsonLine, slug) {
     'data': obstacles
   });
 
-  map.addLayer({
-    'id': 'wheelchair-problem' + '-' + slug,
-    'type': 'symbol',
-    'source': obstaclesSource,
-    'layout': {
-      'icon-image': 'wheelchair-problem',
-      'icon-overlap': 'always'
-    },
-    'filter': ['all',
-               ['has', 'show'],
-               ['==', 'wheelchair', ['get', 'type']],
-               ['==', 'problem', ['get', 'level']]
-              ]
-  });
+  for (var t = 0; t < types.length; t++) {
+    for (var l = 0; l < levels.length; l++) {
+      map.addLayer({
+        'id': types[t] + '-' + levels[l] + '-' + slug,
+        'type': 'symbol',
+        'source': obstaclesSource,
+        'layout': {
+          'icon-image': types[t] + '-' + levels[l],
+          'icon-overlap': 'always'
+        },
+        'filter': ['all',
+                   ['has', 'show'],
+                   ['==', types[t], ['get', 'type']],
+                   ['==', levels[l], ['get', 'level']]
+                  ]
+      });
+    }
+  }
 
-  map.addLayer({
-    'id': 'wheelchair-danger' + '-' + slug,
-    'type': 'symbol',
-    'source': obstaclesSource,
-    'layout': {
-      'icon-image': 'wheelchair-danger',
-      'icon-overlap': 'always'
-    },
-    'filter': ['all',
-               ['has', 'show'],
-               ['==', 'wheelchair', ['get', 'type']],
-               ['==', 'danger', ['get', 'level']]
-              ]
-  });
+  for (var t = 0; t < types.length; t++) {
+    for (var l = 0; l < levels.length; l++) {
+      var layerName = types[t] + '-' + levels[l] + '-' + slug;
 
-  map.addLayer({
-    'id': 'sight-problem' + '-' + slug,
-    'type': 'symbol',
-    'source': obstaclesSource,
-    'layout': {
-      'icon-image': 'sight-problem',
-      'icon-overlap': 'always'
-    },
-    'filter': ['all',
-               ['has', 'show'],
-               ['==', 'sight', ['get', 'type']],
-               ['==', 'problem', ['get', 'level']]
-              ]
-  });
+      map.on('mouseenter', layerName, function (e) {
+        map.getCanvas().style.cursor = 'pointer';
+      });
 
-  map.addLayer({
-    'id': 'sight-danger' + '-' + slug,
-    'type': 'symbol',
-    'source': obstaclesSource,
-    'layout': {
-      'icon-image': 'sight-danger',
-      'icon-overlap': 'always'
-    },
-    'filter': ['all',
-               ['has', 'show'],
-               ['==', 'sight', ['get', 'type']],
-               ['==', 'danger', ['get', 'level']]
-              ]
-  });
+      map.on('mouseleave', layerName, function () {
+        map.getCanvas().style.cursor = '';
+      });
 
-  const layers = [
-    'wheelchair-danger' + '-' + slug,
-    'wheelchair-problem' + '-' + slug,
-    'sight-danger' + '-' + slug,
-    'sight-problem' + '-' + slug
-  ];
-  for (var l = 0; l < layers.length; ++l) {
-    map.on('mouseenter', layers[l], function (e) {
-      map.getCanvas().style.cursor = 'pointer';
-    });
+      map.on('click', layerName, function (e) {
+        if(!e.originalEvent.defaultPrevented) {
+          e.originalEvent.preventDefault();
 
-    map.on('mouseleave', layers[l], function () {
-      map.getCanvas().style.cursor = '';
-    });
+          var coordinates = e.features[0].geometry.coordinates.slice();
+          var html = '<img class="popup-img" src="img/obstacles/' + e.features[0].properties.id + '.jpg">';
 
-    map.on('click', layers[l], function (e) {
-      if(!e.originalEvent.defaultPrevented) {
-        e.originalEvent.preventDefault();
+          // Ensure that if the map is zoomed out such that multiple
+          // copies of the feature are visible, the popup appears
+          // over the copy being pointed to.
+          while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+          }
 
-        var coordinates = e.features[0].geometry.coordinates.slice();
-        var html = '<img class="popup-img" src="img/obstacles/' + e.features[0].properties.id + '.jpg">';
-
-        // Ensure that if the map is zoomed out such that multiple
-        // copies of the feature are visible, the popup appears
-        // over the copy being pointed to.
-        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+          popup.setLngLat(coordinates)
+            .setHTML(html)
+            .addTo(map);
         }
-
-        popup.setLngLat(coordinates)
-          .setHTML(html)
-          .addTo(map);
-      }
-    });
+      });
+    }
   }
 };
 
 var moveLayers = function(map, slug) {
-  if (map.getLayer('wheelchair-danger' + '-' + slug)) {
-    map.moveLayer('wheelchair-danger' + '-' + slug);
-  }
-  if (map.getLayer('wheelchair-problem' + '-' + slug)) {
-    map.moveLayer('wheelchair-problem' + '-' + slug);
-  }
-  if (map.getLayer('sight-danger' + '-' + slug)) {
-    map.moveLayer('sight-danger' + '-' + slug);
-  }
-  if (map.getLayer('sight-problem' + '-' + slug)) {
-    map.moveLayer('sight-problem' + '-' + slug);
+  for (var t = 0; t < types.length; t++) {
+    for (var l = 0; l < levels.length; l++) {
+      var layerName = types[t] + '-' + levels[l] + '-' + slug;
+      if (map.getLayer(layerName)) {
+        map.moveLayer(layerName);
+      }
+    }
   }
 };
 
